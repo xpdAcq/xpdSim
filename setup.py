811e2a14a8f72e4f56e4d94b72a07872f1aa487e
Simon Billinge <billinge@LPM-154661.bnl.gov>
2016-01-21 18:47:24 -0500

    adding simsetup to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'xpdsim',
     version = '1.0.1',
-    packages = ['bluesky', 'ophyd'],
+    packages = ['bluesky', 'ophyd', 'simsetup'],
     description = 'simulators',
     zip_safe = False,
     url = 'https://github.com/chiahaoliu/xpdSim'
