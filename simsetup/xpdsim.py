1d6e400f82b7cc5cc131f30349500e9c1b8d793e
Simon Billinge <billinge@LPM-154661.bnl.gov>
2016-01-21 18:54:09 -0500

    simulation setup testing

--- simsetup/xpdsim.py
+++ simsetup/xpdsim.py
@@ -1,1 +1,10 @@
-print('xpdSim setup')
+import os
+
+def main():
+    '''scripts for setting up the xpd user simulation test frame
+    '''
+    os.mkdir('xpduser')
+
+if __name__ == '__main__':
+    main()
+
