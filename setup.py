e93ccab37b38d27944e128d3d6946165d34e65cf
chiahaoliu <chbrahmsliu@gmail.com>
2016-01-20 19:22:16 -0500

    add zip_safe = False
    prevent ipython bug

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'xpdsim',
-    version = '1.0.0',
+    version = '1.0.1',
     packages = ['bluesky', 'ophyd'],
     description = 'simulators',
+    zip_safe = False,
     url = 'https://github.com/chiahaoliu/xpdSim'
     )
     
