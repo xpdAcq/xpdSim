e35d1201dc21694ab56d781a1782d65d886617bb
Simon Billinge <billinge@LPM-154661.bnl.gov>
2016-01-21 19:03:48 -0500

    added config directory to xpdsim.py

--- simsetup/xpdsim.py
+++ simsetup/xpdsim.py
@@ -4,6 +4,8 @@ def main():
     '''scripts for setting up the xpd user simulation test frame
     '''
     os.mkdir('xpduser')
+    os.mkdir('config')
+    
 
 if __name__ == '__main__':
     main()
