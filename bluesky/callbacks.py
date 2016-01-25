c065c381249aaa86f8e2807f789e961569010529
Simon Billinge <simon.billinge@gmail.com>
2016-01-25 17:27:57 -0500

    fixing copyright and licensing information throughout project

--- bluesky/callbacks.py
+++ bluesky/callbacks.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# callbacks         by Billinge Group
+#                   Simon J. L. Billinge sb2896@columbia.edu
+#                   (c) 2016 trustees of Columbia University in the City of
+#                        New York.
+#                   All rights reserved
+#
+# File coded by:    Timothy Liu
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
 ## fake callback module, mostly following the original strucutre
 class CallbackBase(object):
     def __init__(self):
