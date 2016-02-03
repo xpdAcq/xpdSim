6044e9e9e2d27f61ea7c1761269ff94c8fe67036
Timothy Liu <chbrahmsliu@gmail.com>
2016-02-03 16:15:05 -0500

    finish fake AreaDetector

--- simulator/areadetector.py
+++ simulator/areadetector.py
@@ -1,11 +1,15 @@
 from ophyd import Device
 from ophyd import Signal
 from ophyd import Component as C
-# require real ophyd, prettytable
+# require real ophyd, prettytable, pyepicse
+from ophyd.areadetector import TIFFPlugin
+
+class Cam(Device):
+    acquire_time = C(Signal, value=1, add_prefix=())
 
 
 class AreaDetector(Device):
     images_per_set = C(Signal, value=1, add_prefix=())
     number_of_sets = C(Signal, value=1, add_prefix=())
-
+    cam = C(Cam,'', add_prefix=())
 
