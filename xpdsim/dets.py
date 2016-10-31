dc2613d1e7162118e63c2142d49fd7ad90802375
christopher <cjwright4242@gmail.com>
2016-10-31 15:15:05 -0400

    DOC: add documentation to the cycle builder

--- xpdsim/dets.py
+++ xpdsim/dets.py
@@ -9,10 +9,25 @@ import os
 
 DATA_DIR = rs_fn('xpdsim', 'data/')
 
+
 def build_image_cycle(path):
+    """Build image cycles, essentially generators with endless images
+
+    Parameters
+    ----------
+    path: str
+        Path to the files to be used as the base for the cycle, this can
+        include some globing
+
+    Returns
+    -------
+    Cycler:
+        The iterable like object to cycle through the images
+    """
     imgs = ImageSequence(path)
     return cycler(img=[i for i in imgs])
 
 nsls_ii_ni = build_image_cycle(
     os.path.join(DATA_DIR,
-                 'XPD/ni/sub_nickel_std_ct_for_10_20160309-1742_0.tiff'))
+                 'XPD/ni/*.tiff'))
+print(nsls_ii_ni)
