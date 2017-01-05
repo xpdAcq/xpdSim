1d3e6fe82e037ac14ae1b145b6507393c0dee1e9
christopher <cjwright4242@gmail.com>
2017-01-04 21:24:16 -0500

    MTN: remove copy pasted code

--- xpdsim/tests/conftest.py
+++ xpdsim/tests/conftest.py
@@ -15,20 +15,10 @@
 import os
 import shutil
 import sys
-
-import numpy as np
-import pytest
-
-from xpdan.data_reduction import DataReduction
-from xpdan.glbl import make_glbl
-from xpdan.io import fit2d_save
-from xpdan.simulation import build_pymongo_backed_broker
-from xpdan.tests.utils import insert_imgs
 import tempfile
-from xpdan.fuzzybroker import FuzzyBroker
-from pkg_resources import resource_filename as rs_fn
-import yaml
-from uuid import uuid4
+
+import pytest
+from databroker.tests.conftest import build_pymongo_backed_broker
 
 if sys.version_info >= (3, 0):
     pass
@@ -38,15 +28,6 @@ def clean_database(database):
     for sub_db_name in ['mds', 'fs']:
         sub_db = getattr(database, sub_db_name)
         sub_db._connection.drop_database(sub_db.config['database'])
-
-
-@pytest.fixture(scope='module')
-def mk_glbl(exp_db):
-    a = make_glbl(1, exp_db)
-    yield a
-    if os.path.exists(a.base):
-        print('removing {}'.format(a.base))
-        shutil.rmtree(a.base)
 
 
 @pytest.fixture(params=[
@@ -61,37 +42,6 @@ def db(request):
     yield databroker
     print('CLEAN DB')
     clean_database(databroker)
-
-
-@pytest.fixture(scope='module')
-def img_size():
-    a = np.random.random_integers(100, 200)
-    yield (a, a)
-
-
-@pytest.fixture(scope='module')
-def wavelength():
-    yield 1.15
-
-
-@pytest.fixture(scope='module')
-def disk_mask(tmp_dir, img_size):
-    mask = np.random.random_integers(0, 1, img_size).astype(bool)
-    dirn = tmp_dir
-    file_name_msk = os.path.join(dirn, 'mask_test' + '.msk')
-    assert ~os.path.exists(file_name_msk)
-    fit2d_save(mask, 'mask_test', dirn)
-    assert os.path.exists(file_name_msk)
-    file_name = os.path.join(dirn, 'mask_test' + '.npy')
-    assert ~os.path.exists(file_name)
-    np.save(file_name, mask)
-    assert os.path.exists(file_name)
-    yield (file_name_msk, file_name, mask)
-
-
-@pytest.fixture(scope='module')
-def fuzzdb(exp_db):
-    yield FuzzyBroker(exp_db.mds, exp_db.fs)
 
 
 @pytest.fixture(scope='module')
