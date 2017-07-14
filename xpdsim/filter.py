76f8ca480e8a006ebdf5992ad95144fe535f2404
Brooke Ferber <bferber@stanford.edu>
2017-07-14 16:25:02 -0400

    DEV: created filter objects with attenuation

---
+++ xpdsim/filter.py
@@ -0,0 +1,25 @@
+import bluesky.exammples as be
+
+
+class FilterBank():
+    def __init__(self, attenuations=None):
+        if attenuations is None:
+            attenuations = {'filter1': .5, 'filter2': .5, 'filter3': .5,
+                            'filter4': .5}
+        self.filter_list = []
+        for k, v in attenuations.items():
+            f = Filter(k, {'rad': lambda x: x}, {'x': 0}, v)
+            self.filter_list.append(f)
+            setattr(self, k, f)
+
+    def getAttenuation(self):
+        totalAttenuation = 1
+        for filter in self.filter_list:
+            totalAttenuation *= filter.attenuation
+        return totalAttenuation
+
+
+class Filter(be.Mover):
+    def __init__(self, name, fields, initial_set, attenuation, **kwargs):
+        self.attenuation = attenuation
+        super().__init__(name, fields, initial_set, **kwargs)
--- xpdsim/movers.py
+++ xpdsim/movers.py
@@ -2,3 +2,5 @@ import bluesky.examples as be
 
 shctl1 = be.Mover('shctl1', {'rad': lambda x: x}, {'x': 0})
 cs700 = be.Mover('cs700', {'temperature': lambda x: x}, {'x': 300})
+
+
