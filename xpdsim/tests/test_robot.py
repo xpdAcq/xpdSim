f6656f5a0391462c372cf6e03942680479fe2062
Brooke Ferber <bferber@stanford.edu>
2017-08-03 16:29:24 -0400

    ENH: Added sample num to robot test

--- xpdsim/tests/test_robot.py
+++ xpdsim/tests/test_robot.py
@@ -1,11 +1,15 @@
-from ..robot import Robot
 import bluesky.examples as be
+import pytest
+from ..dets import robot_factory, nsls_ii_path, chess_path, det_factory
+
+test_params = [('nslsii', nsls_ii_path), ('chess', chess_path)]
 
 
+@pytest.mark.parametrize(('name', 'fp'), test_params)
 def test_robot():
     th = be.Mover('theta', {'theta': lambda x: x}, {'x': 0})
-    s_m = {'sample1': 'img1', 'sample2': 'img2'}
-    r = Robot('PV_PREFIX:', th, sample_map=s_m)
-    assert r.sample_map.get('sample1') is 'img1'
-
-
+    s_m = {'nslsii': nsls_ii_path, 'chess': chess_path}
+    r = robot_factory(th, sample_map=s_m)
+    print(r.current_sample_number)
+    assert r.sample_map.get('nslsii') is nsls_ii_path
+    det_factory(name, db.fs, fp, save_path=tmp_dir, Robot=r)
