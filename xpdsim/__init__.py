7994dec79d60d83420239998fb44042c17ef045a
Timothy Liu <chbrahmsliu@gmail.com>
2017-08-21 16:20:50 -0400

    CLN: clean nasty build_db code

--- xpdsim/__init__.py
+++ xpdsim/__init__.py
@@ -1,12 +1,11 @@
 import numpy as np
 import bluesky.examples as be
 
-from xpdsim.build_db import build_pymongo_backed_broker
+from xpdsim.build_db import build_db_wrapper
 from xpdsim.movers import shctl1, cs700
 from xpdsim.dets import SimulatedPE1C
 
-db = build_pymongo_backed_broker()
-db.reg.register_handler('RWFS_NPY', be.ReaderWithRegistryHandler)
-simple_pe1c = SimulatedPE1C('pe1c', {'pe1_image':
-                                     lambda: np.ones((5, 5))},
+db = build_db_wrapper() # default is mongo. build sqlite one by
+                        # specific imports
+simple_pe1c = SimulatedPE1C('pe1c', {'pe1_image':lambda: np.ones((5, 5))},
                             reg=db.reg)
--- xpdsim/build_db.py
+++ xpdsim/build_db.py
@@ -1,28 +1,67 @@
+"""this module exists for simulation environment"""
 import os
 import uuid
 
-from databroker import Broker
-from databroker.headersource.mongo import MDS
-from databroker.assets.utils import create_test_database
-from databroker.assets.mongo import Registry
-from databroker.assets.handlers import NpyHandler
+from databroker import Broker, BrokerES
+#from databroker.assets.handlers import NpyHandler
+from databroker.headersource import HeaderSourceShim
+from databroker.eventsource import EventSourceShim
 
+def build_db_wrapper(option='mongo'):
+    if option == 'mongo':
+        print("BUILD mongo-backed db")
+        db = build_pymongo_backed_broker(None)
+    elif option == 'sqlite':
+        print("BUILD sqlite-backed db")
+        db = build_sqlite_backed_broker(None)
 
-def build_pymongo_backed_broker():
+    return db
+
+def build_sqlite_backed_broker(request):
+    """Uses mongoquery + sqlite -- no pymongo or mongo server anywhere"""
+    from databroker.headersource.sqlite import MDS
+    from databroker.assets.sqlite import Registry
+
+    tempdirname = tempfile.mkdtemp()
+    mds = MDS({'directory': tempdirname,
+               'timezone': tzlocal.get_localzone().zone,
+               'version': 1})
+    filenames = ['run_starts.json', 'run_stops.json', 'event_descriptors.json',
+                 'events.json']
+    for fn in filenames:
+        with open(os.path.join(tempdirname, fn), 'w') as f:
+            f.write('[]')
+
+    def delete_mds():
+        shutil.rmtree(tempdirname)
+
+    #request.addfinalizer(delete_mds)
+
+    tf = tempfile.NamedTemporaryFile()
+    fs = Registry({'dbpath': tf.name})
+
+    def delete_fs():
+        os.remove(tf.name)
+
+    #request.addfinalizer(delete_fs)
+
+    return BrokerES(HeaderSourceShim(mds),
+                    [EventSourceShim(mds, fs)],
+                    {'': fs})
+
+def build_pymongo_backed_broker(request):
     '''Provide a function level scoped MDS instance talking to
     temporary database on localhost:27017 with v1 schema.
 
     '''
+    from databroker.headersource.mongo import MDS
+    from databroker.assets.utils import create_test_database
+    from databroker.assets.mongo import Registry
 
     db_name = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
     md_test_conf = dict(database=db_name, host='localhost',
                         port=27017, timezone='US/Eastern',
                         version=1)
-    #try:
-        # nasty details: to save MacOS user
-    #    mds = MDS(md_test_conf, 1, auth=False)
-    #except TypeError:
-    #    mds = MDS(md_test_conf, auth=False)
     mds = MDS(md_test_conf, auth=False)
 
     db_name = "fs_testing_base_disposable_{uid}"
@@ -30,12 +69,12 @@ def build_pymongo_backed_broker():
                                         port=27017, version=1,
                                         db_template=db_name)
     fs = Registry(fs_test_conf)
-    fs.register_handler('npy', NpyHandler)
 
     def delete_fs():
         print("DROPPING DB")
         fs._connection.drop_database(fs_test_conf['database'])
         mds._connection.drop_database(md_test_conf['database'])
 
+    #request.addfinalizer(delete_fs)
 
     return Broker(mds, fs)
