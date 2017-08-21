c2dcc272d3a8689e3887d52c604611d6408d9693
Timothy Liu <chbrahmsliu@gmail.com>
2017-08-21 17:28:29 -0400

    CLN: rename module

--- xpdsim/__init__.py
+++ xpdsim/__init__.py
@@ -1,11 +1,10 @@
 import numpy as np
 import bluesky.examples as be
 
-from xpdsim.build_db import build_db_wrapper
+from xpdsim.build_sim_db import build_sim_db
 from xpdsim.movers import shctl1, cs700
 from xpdsim.dets import SimulatedPE1C
 
-db = build_db_wrapper() # default is mongo. build sqlite one by
-                        # specific imports
+db = build_sim_db() # default is sqlite
 simple_pe1c = SimulatedPE1C('pe1c', {'pe1_image':lambda: np.ones((5, 5))},
                             reg=db.reg)
