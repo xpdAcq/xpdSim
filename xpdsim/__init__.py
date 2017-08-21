f9c7b6fe239f46b209dda6e8b5ec09f123f14d29
Timothy Liu <chbrahmsliu@gmail.com>
2017-08-21 18:19:59 -0400

    FIX: add handler

--- xpdsim/__init__.py
+++ xpdsim/__init__.py
@@ -6,5 +6,6 @@ from xpdsim.movers import shctl1, cs700
 from xpdsim.dets import SimulatedPE1C
 
 db = build_sim_db() # default is sqlite
+db.reg.register_handler('RWFS_NPY', be.ReaderWithRegistryHandler)
 simple_pe1c = SimulatedPE1C('pe1c', {'pe1_image':lambda: np.ones((5, 5))},
                             reg=db.reg)
