2fa2e647689f8100c69421a8d828a23940487d4b
Timothy Liu <chbrahmsliu@gmail.com>
2017-08-22 19:41:14 -0400

    ENH: instantiate realistic area_det at top level
    Realistic area_det spills out real image and has proper shutter logic

--- xpdsim/__init__.py
+++ xpdsim/__init__.py
@@ -3,9 +3,11 @@ import bluesky.examples as be
 
 from xpdsim.build_sim_db import build_sim_db
 from xpdsim.movers import shctl1, cs700
-from xpdsim.dets import SimulatedPE1C
+from xpdsim.dets import *
 
 db = build_sim_db() # default is sqlite
 db.reg.register_handler('RWFS_NPY', be.ReaderWithRegistryHandler)
 simple_pe1c = SimulatedPE1C('pe1c', {'pe1_image':lambda: np.ones((5, 5))},
                             reg=db.reg)
+# advanced detector
+xpd_pe1c = det_factory('pe1c', db.reg, nsls_ii_path, shutter=shctl1)
