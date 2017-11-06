99132285964f4b40245796c1eae1d539aff6c5eb
Timothy Liu <chbrahmsliu@gmail.com>
2017-11-06 14:38:25 -0500

    DEV: add readback names to devices

--- xpdsim/movers.py
+++ xpdsim/movers.py
@@ -1,9 +1,6 @@
 from ophyd.sim import SynAxis
 
-# NOTE: device field name has been deprecated. The readback field is
-# default to device name for all devices.
-#shutter_read_field = 'rad'
-#temp_controller_read_field = 'temperature'
-
 cs700 = SynAxis(name='cs700', value=300)
+cs700.readback.name = 'temperature'
 shctl1 = SynAxis(name='shctl1', value=0)
+shctl1.readback.name = 'rad'
