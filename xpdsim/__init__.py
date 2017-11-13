6d3a290236e7c67bd19e0ffff098d1787f10199d
Timothy Liu <chbrahmsliu@gmail.com>
2017-11-13 14:34:57 -0500

    DEV: add simulated ring_current

--- xpdsim/__init__.py
+++ xpdsim/__init__.py
@@ -2,13 +2,16 @@ import numpy as np
 
 from xpdsim.build_sim_db import build_sim_db
 from xpdsim.movers import shctl1, cs700
-from ophyd.sim import SynSignalWithRegistry
-from xpdsim.area_det import det_factory
+from ophyd.sim import (SynSignalWithRegistry, NumpySeqHandler,
+                       SynSignalRO)
+from xpdsim.area_det import det_factory, nsls_ii_path, xpd_wavelength
 
 db_path, db = build_sim_db() # default is sqlite
 db.reg.register_handler('NPY_SEQ', NumpySeqHandler)
-#simple_pe1c = SimulatedPE1C('pe1c', {'pe1_image':lambda: np.ones((5, 5))},
-#                            reg=db.reg)
-# advanced detector
-#xpd_pe1c = det_factory('pe1c', db.reg, nsls_ii_path, shutter=shctl1)
-
+# detector with 5 by 5 image -> for testing functionality
+simple_pe1c = det_factory(db.reg)
+# detector with full image -> for testing data reduction
+xpd_pe1c = det_factory(db.reg, full_img=True,
+                       src_path=nsls_ii_path)
+# synthetic ring current
+ring_current = SynSignalRO(lambda : 300, name='ring_current')
