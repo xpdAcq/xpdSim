00cc93b67a5947d59b7d7202c8fe129618518a52
Timothy Liu <chbrahmsliu@gmail.com>
2018-01-10 17:09:56 -0500

    TST: add test on movers, including filter bank

---
+++ xpdsim/tests/test_movers.py
@@ -0,0 +1,27 @@
+from xpdsim.movers import *
+
+def test_cs700():
+    # simple test if the object behaves as default
+    from xpdsim.movers import cs700
+    assert cs700.readback.name == 'temperature'
+    cs700.set(310)
+    assert cs700.readback.value == 310
+
+
+def test_shctl1():
+    # simple test if the object behaves as default
+    from xpdsim.movers import shctl1
+    assert shctl1.readback.name == 'rad'
+    shctl1.set(60)
+    assert shctl1.readback.value == 60
+
+
+def test_filterbank():
+    # simple test if the object behaves as default
+    from xpdsim.movers import fb
+    in_flts = [ x for x in fb.component_names if x.endswith('1')]
+    out_flts = [ x for x in fb.component_names if x not in in_flts]
+    for flt in out_flts:
+        assert getattr(fb, flt).value == 'Out'
+    for flt in in_flts:
+        assert getattr(fb, flt).value == 'In'
