21b5d3d74c363d53d9b39a9ae4022825eb39e1ce
Christopher J. Wright <cjwright4242@gmail.com>
2019-03-20 10:05:08 -0400

    use shutter and noise

--- xpdsim/tests/test_dets.py
+++ xpdsim/tests/test_dets.py
@@ -88,7 +88,7 @@ def test_dets_noise(RE, db, name, fp):
 
 
 def test_dexela(RE, db):
-    det = det_factory_dexela(db.reg)
+    det = det_factory_dexela(db.reg, shutter=shctl1, noise=True)
     RE.subscribe(db.insert, "all")
     RE(bs.abs_set(shctl1, XPD_SHUTTER_CONF["open"], wait=True))
     uid = RE(bp.count([det]))
