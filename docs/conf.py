627e60904d2c27dafde0d1abbb58421b9f4567fb
Christopher J. Wright <cjwright4242@gmail.com>
2019-08-30 12:51:29 -0400

    back off rtd for now

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
+#import sphinx_rtd_theme
+#html_theme = "sphinx_rtd_theme"
 extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode']
