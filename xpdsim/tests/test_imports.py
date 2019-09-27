a2904743ef0a98809fa84f425d2517082f42df43
Timothy Liu <chbrahmsliu@gmail.com>
2019-09-27 12:40:49 -0400

    TST: check simulated objects in xpdsim

--- xpdsim/tests/test_imports.py
+++ xpdsim/tests/test_imports.py
@@ -2,7 +2,18 @@ import pyFAI
 
 from xpdsim import *
 
+DEVICE_LIST = ['simple_pe1c',
+               'xpd_pe1c',
+               'xpd_pe2c',
+               'dexela',
+               'blackfly',
+               'blackfly_full_field',
+               'ring_current',
+               ]
 
 def test_load_pyfai():
     print(pyfai_poni)
     assert pyFAI.load(pyfai_poni)
+
+def test_all_devices():
+    assert all([x in globals() for x in DEVICE_LIST])
